4065c9d1572d0ab18e1b18504aa5705aac2cd324
Denis Batista de Andrade <deniszola@gmail.com>
2019-08-05 19:49:24 -0300

    scroll-to-top done and minor updates

---
+++ app/scripts/main.js
@@ -0,0 +1,17 @@
+// =================================
+// ==========scroll-to-top==========
+// =================================
+window.onscroll = function () {
+  let scroll_to_top = document.getElementById("scroll-to-top")
+
+  if (document.documentElement.scrollTop > 200) {
+    scroll_to_top.style.display = "block"
+  } else {
+    scroll_to_top.style.display = "none"
+  }
+}
+
+// ================================
+function scrollToTop() {
+  document.documentElement.scrollTop = 0
+}
